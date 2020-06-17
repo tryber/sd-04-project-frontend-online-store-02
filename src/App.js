35a6f250f1d65270b493fe0167a88dc39d9125ac
binhus <eng.robsonwilli@gmail.com>
2020-06-17 18:08:35 -0300

    componentes do header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import ProductList from './components/ProductList';
+import Header from './components/header/Header';
 
 function App() {
   return (
     <div className="App">
       {/* ProductList tem que receber os dados via props "productsData" */}
+      <Header />
       <ProductList />
     </div>
   );
