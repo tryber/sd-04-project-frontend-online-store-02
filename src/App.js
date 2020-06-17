83b3d5e3b8e9ebd8c68f55f1bd65e782dd701656
binhus <eng.robsonwilli@gmail.com>
2020-06-17 18:30:35 -0300

    header components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import ProductList from './components/ProductList';
+import Aside from './components/Aside';
 import Header from './components/header/Header';
 
-function App() {
-  return (
-    <div className="App">
-      {/* ProductList tem que receber os dados via props "productsData" */}
-      <Header />
-      <ProductList />
-    </div>
-  );
+class App extends Component {
+  // click() {}
+  render() {
+    return (
+      <div className="App">
+        {/* ProductList tem que receber os dados via props "productsData" */}
+        <Header />
+        <ProductList />
+        <Aside click={
+this.click
+} />
+      </div>
+    );
+  }
 }
 
 export default App;
