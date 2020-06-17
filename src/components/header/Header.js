362718ac0b30d61ca6e7832c8e585863d40cece8
binhus <eng.robsonwilli@gmail.com>
2020-06-17 18:40:34 -0300

    CC issue

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,11 +2,12 @@ import React from 'react';
 import Input from '../cardComponents/input';
 import Button from '../cardComponents/button';
 import Image from '../cardComponents/Image';
+//import CardButton from '../cardComponents/CardComponents'
 import './Header.css';
 
 export default class Header extends React.Component {
   render() {
-    const { count, inputValue } = this.props;
+    const { inputValue } = this.props;
     return (
       <header>
         <Image
@@ -16,6 +17,7 @@ export default class Header extends React.Component {
         <div>
           <Input type="text" value={inputValue} test="query-input" />
           <Button text="Pesquisar" test="query-button" />
+          {/*<CardButton count={count} />*/}
         </div>
       </header>
     );
