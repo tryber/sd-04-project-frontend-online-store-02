4390ca10e88d91593c83b209868a7749a58cb65b
Diego Campos <diegodiko05@gmail.com>
2020-06-17 15:19:22 -0300

    Iniciando o componente aSide que vai conter as categorias

---
+++ src/components/aSide.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { getCategories } from '../services/api'
+
+class aSide extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+export default aSide;
