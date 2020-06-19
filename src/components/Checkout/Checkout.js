d32b4154651288e5bd8e2a03dc11a53f98adeba3
binhus <eng.robsonwilli@gmail.com>
2020-06-19 17:31:38 -0300

    CC issues

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -6,18 +6,18 @@ class Checkout extends Component {
     return (
       <div>
         <form>
-          <label for="fullName">Nome Completo</label>
+          <label htmlFor="fullName">Nome Completo</label>
           <Input type="text" name="fullName" test="checkout-fullname" />
-          <label for='email'>Email</label>
-          <Input type='text' name='email' test='checkout-email' />
-          <label for='cpf'>CPF</label> 
-          <Input type='text' name='cpf' test='checkout-cpf' />
-          <label for='telefone'>Telefone</label> 
-          <Input type='text' name='telefone' test='checkout-phone' />
-          <label for='cep'>CEP</label> 
-          <Input type='text' name='cep' test='checkout-cep' />
-          <label for='address'>CPF</label> 
-          <Input type='text' name='address' test='checkout-address' />
+          <label htmlFor="email">Email</label>
+          <Input type="text" name="email" test="checkout-email" />
+          <label htmlFor="cpf">CPF</label>
+          <Input type="text" name="cpf" test="checkout-cpf" />
+          <label htmlFor="telefone">Telefone</label>
+          <Input type="text" name="telefone" test="checkout-phone" />
+          <label htmlFor="cep">CEP</label>
+          <Input type="text" name="cep" test="checkout-cep" />
+          <label htmlFor="address">CPF</label>
+          <Input type="text" name="address" test="checkout-address" />
         </form>
       </div>
     );
