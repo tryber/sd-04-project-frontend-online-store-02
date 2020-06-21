63a598697ab29a109f22c6b41cc1e785b2dc6723
Diego Campos <diegodiko05@gmail.com>
2020-06-20 23:53:45 -0300

    Requisito 15, colocando a informaçao de frete gratis tambem no Product Details

--- src/components/ProductDetails/ProductDetails.js
+++ src/components/ProductDetails/ProductDetails.js
@@ -2,24 +2,24 @@ import React from 'react';
 import * as api from '../../services/api';
 import Image from '../cardComponents/Image';
 import FormAvaliation from './FormAvaliation';
+import FreeShipping from '../MainContent/Product List-Card/FreeShipping';
 
 class ProductDetails extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { data: [] };
+    this.state = { data: [], bollean: false };
   }
 
   componentDidMount() {
     const { match } = this.props;
-    api
-      .getProductsFromCategoryAndQuery(match.params.id, match.params.id2)
-      .then((data) => {
-        this.setState({ data: data.results[0] });
-      });
+    api.getProductsFromCategoryAndQuery(match.params.id, match.params.id2).then((data) => {
+      this.setState({ data: data.results[0], bollean: data.results[0].shipping.free_shipping });
+    });
   }
 
   render() {
-    const { data } = this.state;
+    const { data, bollean } = this.state;
+    console.log(bollean);
     const { addProductCart } = this.props;
     const object = {
       [data.id]: 1,
@@ -33,11 +33,8 @@ class ProductDetails extends React.Component {
         <h2 data-testid="product-detail-name">{data.title}</h2>
         <p>{data.price}</p>
         <Image src={data.thumbnail} alt={data.title} />
-        <FormAvaliation
-          obj={object}
-          data={data}
-          addProductCart={addProductCart}
-        />
+        <FreeShipping verify={bollean} />
+        <FormAvaliation obj={object} data={data} addProductCart={addProductCart} />
       </React.Fragment>
     );
   }
